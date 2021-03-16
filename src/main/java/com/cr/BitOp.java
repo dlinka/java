a964c5c41c6833c515a183e62e8f5e182e7cfe2f
cyg <dlinka.cg@gmail.com>
2021-03-16 15:54:51 +0800

    update

--- src/main/java/com/cr/BitOp.java
+++ src/main/java/com/cr/BitOp.java
@@ -3,16 +3,47 @@ package com.cr;
 public class BitOp {
 
     public static void main(String[] args) {
-        BitOp bo = new BitOp();
-        bo.bitAND();
-        bo.bitOR();
-        bo.bitXOR();
+        //leftMove();
+        //rightMove();
+        noSymbolRightMove();
+    }
+
+    //补码进行左移n位
+    //符号位也移动
+    //右边补零
+    //移动的位数如果大于等于32位,就对32取余(移动32位,相当于不移动,移动40位,相当于移动8位)
+    public static void leftMove() {
+        int num = 12333292;
+        System.out.println(num << 8);
+        System.out.println(Integer.toBinaryString(num));
+        System.out.println(Integer.toBinaryString(num << 8));
+    }
+
+    //补码进行右移n位
+    //左边补上符号位
+    //正数全补上0,负数全补上1
+    //移动的位数如果大于等于32位,就对32取余(移动32位,相当于不移动,移动40位,相当于移动8位)
+    public static void rightMove() {
+        int num = 12333292;
+        System.out.println(num >> 8);
+        System.out.println(Integer.toBinaryString(num));
+        System.out.println(Integer.toBinaryString(num >> 8));
+    }
+
+    //补码进行右移n位
+    //左边全补上0
+    //移动的位数如果大于等于32位,就对32取余(移动32位,相当于不移动,移动40位,相当于移动8位)
+    public static void noSymbolRightMove() {
+        int num = -1;
+        System.out.println(num >>> 8);
+        System.out.println(Integer.toBinaryString(num));
+        System.out.println(Integer.toBinaryString(num >>> 8));
     }
 
     //与运算
     //&
     //两个位都为1时,才为1
-    public void bitAND() {
+    public static void bitAND() {
         System.out.println(1 & 1); //1
         System.out.println(1 & 0); //0
         System.out.println(0 & 0); //0
@@ -21,7 +52,7 @@ public class BitOp {
     //或运算
     //|
     //两个位都为0时,才为0
-    public void bitOR() {
+    public static void bitOR() {
         System.out.println(1 | 1); //1
         System.out.println(1 | 0); //1
         System.out.println(0 | 0); //0
@@ -30,7 +61,7 @@ public class BitOp {
     //异或运算
     //^
     //两个位相同为0,相异为1
-    public void bitXOR() {
+    public static void bitXOR() {
         System.out.println(1 ^ 1); //0
         System.out.println(0 ^ 0); //0
         System.out.println(1 ^ 0); //1
