a4706fc14eee48fda4c49da43ff35d3b8010e28c
cyg <dlinka.cg@gmail.com>
2021-03-16 14:58:56 +0800

    init

---
+++ src/main/java/com/cr/BitOp.java
@@ -0,0 +1,39 @@
+package com.cr;
+
+public class BitOp {
+
+    public static void main(String[] args) {
+        BitOp bo = new BitOp();
+        bo.bitAND();
+        bo.bitOR();
+        bo.bitXOR();
+    }
+
+    //与运算
+    //&
+    //两个位都为1时,才为1
+    public void bitAND() {
+        System.out.println(1 & 1); //1
+        System.out.println(1 & 0); //0
+        System.out.println(0 & 0); //0
+    }
+
+    //或运算
+    //|
+    //两个位都为0时,才为0
+    public void bitOR() {
+        System.out.println(1 | 1); //1
+        System.out.println(1 | 0); //1
+        System.out.println(0 | 0); //0
+    }
+
+    //异或运算
+    //^
+    //两个位相同为0,相异为1
+    public void bitXOR() {
+        System.out.println(1 ^ 1); //0
+        System.out.println(0 ^ 0); //0
+        System.out.println(1 ^ 0); //1
+    }
+
+}
