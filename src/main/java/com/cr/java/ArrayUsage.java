7d12f28fb0c161dc7495243b5bd9e7b01ecf86cf
CR <dlinka.cg@gmail.com>
2021-10-20 13:39:17 +0800

    数组

---
+++ src/main/java/com/cr/java/ArrayUsage.java
@@ -0,0 +1,15 @@
+package com.cr.java;
+
+public class ArrayUsage {
+
+    public static void main(String[] args) {
+        //静态初始化
+        Integer[] integers1 = new Integer[]{1, 2, 3, 4, 5};
+        //简化静态初始化
+        Integer[] integers2 = {1, 2, 3, 4, 5};
+
+        //动态初始化
+        Integer[] integers3 = new Integer[5];
+    }
+
+}
