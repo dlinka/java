c73db28aca69cb8280f717c4facf42db422494a2
CR <dlinka.cg@gmail.com>
2021-10-20 20:35:36 +0800

    CopyOnWriteArrayList

---
+++ src/main/java/com/cr/jcf/list/CopyOnWriteArrayListDemo.java
@@ -0,0 +1,42 @@
+package com.cr.jcf.list;
+
+import com.cr.common.Facility;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * 1.测试并发读写
+ */
+public class CopyOnWriteArrayListDemo {
+
+    public static void concurrentRW() {
+        CopyOnWriteArrayList<Integer> list = new CopyOnWriteArrayList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        Thread thread1 = new Thread(() -> {
+            for (Integer i : list) {
+                Facility.print("read - {}", i);
+                Facility.sleep(1);
+            }
+        });
+
+        Thread thread2 = new Thread(() -> {
+            int i = 6;
+            list.add(i);
+            Facility.print("write - {}", i);
+        });
+
+        thread1.start();
+        thread2.start();
+    }
+
+    public static void main(String[] args) {
+        CopyOnWriteArrayListDemo demo = new CopyOnWriteArrayListDemo();
+        demo.concurrentRW();
+    }
+
+}
