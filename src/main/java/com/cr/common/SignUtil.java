c1e1da8fbef187caaa68b5db1798b34dde640161
CR <dlinka.cg@gmail.com>
2022-05-14 16:11:02 +0800

    sign签名

--- src/main/java/com/cr/common/SignUtil.java
+++ src/main/java/com/cr/common/SignUtil.java
@@ -1,6 +1,7 @@
 package com.cr.common;
 
 import cn.hutool.crypto.digest.DigestUtil;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 
 import java.util.*;
@@ -9,7 +10,7 @@ import static com.cr.common.Facility.print;
 
 public class SignUtil {
 
-    public static String genSign(Map<String, String> param) {
+    public static String genSign(Map<String, Object> param) {
         StringBuilder builder = new StringBuilder();
         TreeMap map = new TreeMap(param);
         map.forEach((k, v) -> {
@@ -20,10 +21,16 @@ public class SignUtil {
     }
 
     public static void main(String[] args) {
-        Map<String, String> map = new HashMap<>();
-        map.put("deviceId", "123");
-        map.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        Map<String, Object> map = new HashMap<>();
+        map.put("deviceId", 123L);
+        map.put("timestamp", System.currentTimeMillis());
         map.put("appKey", "asdoifjsado");
-        genSign(map);
+        map.put("sign", genSign(map));
+
+
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.putAll(map);
+        print(jsonObject.toString());
+
     }
 }
