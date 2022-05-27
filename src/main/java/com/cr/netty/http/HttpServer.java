9ccea0ca37ed4643c8772461efd647fe4c731058
ts <dlinka.cg@gmail.com>
2022-05-27 19:29:07 +0800

    reimport

--- src/main/java/com/cr/netty/http/HttpServer.java
+++ src/main/java/com/cr/netty/http/HttpServer.java
@@ -1,11 +1,9 @@
 package com.cr.netty.http;
 
 import io.netty.bootstrap.ServerBootstrap;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.HttpServerCodec;
